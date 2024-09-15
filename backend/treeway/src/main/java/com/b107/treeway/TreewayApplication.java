358cc5b3465b3accfd7e935430752300b2bc5b83
김인제 <kij723@naver.com>
2024-09-15 14:41:25 +0900

    init: init backend

---
+++ backend/treeway/src/main/java/com/b107/treeway/TreewayApplication.java
@@ -0,0 +1,13 @@
+package com.b107.treeway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreewayApplication.class, args);
+    }
+
+}
---
+++ backend/treeway/src/main/java/com/b107/treeway/sample/Sample.java
@@ -0,0 +1,13 @@
+package com.b107.treeway.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+    @GetMapping("sample")
+    public String testWord() {
+        return "hi";
+    }
+}
