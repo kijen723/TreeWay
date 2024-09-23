b9c9cc65a2b3d29d1e50730e20056e701ba25f98
kinterlocked <dusehd0525@naver.com>
2024-09-23 13:14:59 +0900

    fix: 경로 수정

--- frontend/treeway/next.config.mjs
+++ frontend/treeway/next.config.mjs
@@ -9,5 +9,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default withPWA({
     dest : "public",
-    assetPrefix: isProd ? '/.next' : '',
+    assetPrefix: isProd ? '/_next' : '',
 })(nextConfig)
