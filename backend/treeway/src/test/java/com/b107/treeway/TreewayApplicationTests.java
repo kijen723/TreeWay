1c918d1bfe21ba3aa80406f5ea916c0762d2056f
김인제 <kij723@naver.com>
2024-09-24 16:13:50 +0900

    init: application setting test

--- backend/treeway/src/test/java/com/b107/treeway/TreewayApplicationTests.java
+++ backend/treeway/src/test/java/com/b107/treeway/TreewayApplicationTests.java
@@ -3,11 +3,11 @@ package com.b107.treeway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TreewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class TreewayApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
