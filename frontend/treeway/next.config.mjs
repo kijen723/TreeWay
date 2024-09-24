d36d670997e3bc929e614eb7060e03eb1423e6c6
kinterlocked <dusehd0525@naver.com>
2024-09-24 16:17:24 +0900

    fix : 수정

--- frontend/treeway/next.config.mjs
+++ frontend/treeway/next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 import withPWA from "@ducanh2912/next-pwa";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     reactStrictMode : true,
+    assetPrefix: isProd ? '/_next' : '',
 };
-
-// const isProd = process.env.NODE_ENV === 'production';
 
 export default withPWA({
     dest : "public",
-    // assetPrefix: isProd ? '/_next' : '',
 })(nextConfig)
