0500003f5f0d640960b4a6028c9a51bed81f480b
kinterlocked <dusehd0525@naver.com>
2024-09-24 17:50:39 +0900

    fix : 배포환경 수정

--- frontend/treeway/next.config.mjs
+++ frontend/treeway/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 import withPWA from "@ducanh2912/next-pwa";
 
+<<<<<<< HEAD
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
@@ -11,3 +12,15 @@ const nextConfig = {
 export default withPWA({
     dest : "public",
 })(nextConfig)
+=======
+const isProd = process.env.NODE_ENV === 'production';  // isProd 변수를 먼저 선언
+
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: isProd ? '/_next' : '',  // isProd 변수를 사용
+};
+
+export default withPWA({
+    dest: "public",
+})(nextConfig);  // withPWA에 nextConfig만 넘김
+>>>>>>> e381380 (fix : 배포환경 수정)
