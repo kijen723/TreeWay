e751bdbf3ab9e8ce25208eef39c0d6f0d510f5f3
김인제 <kij723@naver.com>
2024-09-24 16:05:17 +0900

    Update backend/treeway/src/test/java/com/b107/treeway/TreewayApplicationTests.java

--- backend/treeway/src/test/java/com/b107/treeway/TreewayApplicationTests.java
+++ backend/treeway/src/test/java/com/b107/treeway/TreewayApplicationTests.java
@@ -3,6 +3,7 @@ package com.b107.treeway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+<<<<<<< HEAD
 //@SpringBootTest
 //class TreewayApplicationTests {
 //
@@ -11,3 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 //    }
 //
 //}
+=======
+// @SpringBootTest
+// class TreewayApplicationTests {
+
+//     @Test
+//     void contextLoads() {
+//     }
+
+// }
+>>>>>>> 663c51d (Update backend/treeway/src/test/java/com/b107/treeway/TreewayApplicationTests.java)
