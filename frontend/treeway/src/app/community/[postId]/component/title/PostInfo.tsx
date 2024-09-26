bbaa715a1294ba50609099834894fdaabf9f3142
정다운 <dawoon221@naver.com>
2024-09-26 15:13:19 +0900

    Design: 커뮤니티 제목 컴포넌트 css 수정

--- frontend/treeway/src/app/community/[postId]/component/title/PostInfo.tsx
+++ frontend/treeway/src/app/community/[postId]/component/title/PostInfo.tsx
@@ -3,7 +3,7 @@ import styles from '@/app/community/[postId]/page.module.scss'
 export default function PostInfo() {
     return (
         <div className={styles.postInfo}>
-            <h1>제목</h1>
+            <h2>제목</h2>
             <div className={styles.info}>
                 <p>작성자</p>
                 <p>작성날짜</p>
--- frontend/treeway/src/app/community/[postId]/page.tsx
+++ frontend/treeway/src/app/community/[postId]/page.tsx
@@ -1,14 +1,23 @@
 'use client'
 
+import SideList from '@/app/common/Side/SideList';
 import Comments from './component/title/Comments';
 import PostDetail from './component/title/PostDetail';
 import PostSummary from './component/title/PostSummary'
 import styles from './page.module.scss'
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import SmallSortList from '@/app/main/components/SmallSortList';
 
 export default function CommunityDetail() {
+    const sideState = useSelector((state: RootState) => state.sidecontrol);
+
     return (
         <div className={styles.background}>
             <div className={styles.contentsArea}>
+                <div>
+                    <SideList sideState={sideState.value} items={<SmallSortList />}/>
+                </div>
                 <div>
                     <PostSummary />
                     <Comments />
