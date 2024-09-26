209cd42c581c6b9a6fc3ae046dccf80f7e08bd2f
정다운 <dawoon221@naver.com>
2024-09-26 17:09:23 +0900

    feat: 글쓰기 버튼 추가

--- frontend/treeway/src/app/community/component/UpperNav.tsx
+++ frontend/treeway/src/app/community/component/UpperNav.tsx
@@ -1,10 +1,22 @@
+import Button from '@/app/common/Button';
 import styles from '../page.module.scss'
 import SearchBtn from './SearchBtn';
 import SortBox from './SortBox';
 
+import { useRouter } from 'next/navigation';
+
 export default function UpperNav() {
+    const router = useRouter();
+
+    const handleCreatePost = () => {
+        router.push('/community/createPost');
+    }
+
     return (
         <div className={styles.upperNav}>
+            <div className={styles.leftBlock}>
+                <Button content="글쓰기" size="medium" colorType="yellow" onClick={handleCreatePost}/>
+            </div>
             <div className={styles.rightBlock}>
                 <SortBox />
                 <SearchBtn />
---
+++ frontend/treeway/src/app/community/createPost/page.tsx
@@ -0,0 +1,7 @@
+export default function CreatePost() {
+    return (
+        <div>
+            글 작성 페이지
+        </div>
+    );
+}
