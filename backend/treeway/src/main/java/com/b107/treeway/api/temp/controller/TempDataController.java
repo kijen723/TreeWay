4ad77ecc5828368e259f33afdf0c6a0e54f65539
김인제 <kij723@naver.com>
2024-10-01 20:12:30 +0900

    feat: temp api findbyid

--- backend/treeway/src/main/java/com/b107/treeway/api/temp/controller/TempDataController.java
+++ backend/treeway/src/main/java/com/b107/treeway/api/temp/controller/TempDataController.java
@@ -4,11 +4,14 @@ package com.b107.treeway.api.temp.controller;
 import com.b107.treeway.api.temp.entity.TempData;
 import com.b107.treeway.api.temp.service.TempDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/temp")
@@ -22,5 +25,12 @@ public class TempDataController {
         List<TempData> dataList = tempDataService.getAllData();
         System.out.println("데이터 목록: " + dataList); // 데이터 출력
         return dataList;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TempData> getTempDataById(@PathVariable Long id) {
+        Optional<TempData> tempData = tempDataService.getTempDataById(id);
+        return tempData.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- backend/treeway/src/main/java/com/b107/treeway/api/temp/service/TempDataService.java
+++ backend/treeway/src/main/java/com/b107/treeway/api/temp/service/TempDataService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TempDataService {
@@ -15,5 +16,9 @@ public class TempDataService {
     // 모든 데이터를 조회하는 메서드
     public List<TempData> getAllData() {
         return tempDataRepository.findAll();
+    }
+
+    public Optional<TempData> getTempDataById(Long id) {
+        return tempDataRepository.findById(id);
     }
 }
