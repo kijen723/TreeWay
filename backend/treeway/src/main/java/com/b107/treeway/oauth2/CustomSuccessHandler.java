d9bb8f688cfbce0ce4af57958e3390eae8338d26
강지우 <jiu6525@daum.net>
2024-10-06 01:33:56 +0900

    fix : 쿠키 설정 변경

--- backend/treeway/src/main/java/com/b107/treeway/oauth2/CustomSuccessHandler.java
+++ backend/treeway/src/main/java/com/b107/treeway/oauth2/CustomSuccessHandler.java
@@ -64,7 +64,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60*60*60);
-        //cookie.setSecure(true);
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
 
