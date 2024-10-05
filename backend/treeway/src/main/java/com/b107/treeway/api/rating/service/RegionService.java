3bc05137b4fd216b85e6da08e88b8ec70ea3be31
김인제 <kij723@naver.com>
2024-10-05 22:55:52 +0900

    feat: get region api

--- backend/treeway/src/main/java/com/b107/treeway/api/rating/service/RegionService.java
+++ backend/treeway/src/main/java/com/b107/treeway/api/rating/service/RegionService.java
@@ -3,18 +3,19 @@ package com.b107.treeway.api.rating.service;
 import com.b107.treeway.api.rating.entity.Region;
 import com.b107.treeway.api.rating.repository.RegionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class RegionService {
 
-    private final RegionRepository RegionRepository;
+    @Autowired
+    private RegionRepository regionRepository;
 
-    public List<Region> findAll() {
-        List<Region> list = RegionRepository.findAll();
-        return list;
+    public List<Region> getAllRegions() {
+        return regionRepository.findAll();
     }
+
 }
