ae19e11f6ef036d36fe3fb3ce4423be11fdcd1e5
김인제 <kij723@naver.com>
2024-10-06 15:54:31 +0900

    delete: delete temp

--- backend/treeway/src/main/java/com/b107/treeway/api/temp/repository/TempDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.b107.treeway.api.temp.repository;
-
-import com.b107.treeway.api.temp.entity.TempData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TempDataRepository extends JpaRepository<TempData, Long> {
-}
