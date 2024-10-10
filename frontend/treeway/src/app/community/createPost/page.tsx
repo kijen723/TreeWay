58cbac13faf95cb36bdf6cd922f78893d8482649
정다운 <dawoon221@naver.com>
2024-10-10 15:57:34 +0900

    fix: 빌드 에러 수정

--- frontend/treeway/src/app/community/createPost/page.tsx
+++ frontend/treeway/src/app/community/createPost/page.tsx
@@ -10,23 +10,10 @@ import { useRouter } from 'next/navigation';
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import Swal from 'sweetalert2';
+import { b64toFile } from '@/util/fileUtils';
 
 import { regionOptions } from '@/../public/data/region';
 import { industryDetailOptions } from '@/../public/data/industry_detail';
-
-// base64 -> File 변환
-export const b64toFile = (b64Data: string, filename: string) => {
-  const bstr = atob(b64Data);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-
-  while (n > 0) {
-    n -= 1;
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-
-  return new File([u8arr], filename, { type: 'image/png' });
-};
 
 const extractBase64Data = (imgSrc: string) => {
   const base64Pattern = /^data:image\/(png|jpeg|jpg);base64,/;
