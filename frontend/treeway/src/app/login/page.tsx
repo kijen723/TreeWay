ed6c04fdd04c1a694a63bb0d7f82450401e4a1c6
정다운 <dawoon221@naver.com>
2024-10-10 10:20:04 +0900

    feat: 로그인 여부에 따른 페이지 접근권한 설정

--- frontend/treeway/src/app/login/page.tsx
+++ frontend/treeway/src/app/login/page.tsx
@@ -1,7 +1,20 @@
+'use client'
+
 import styles from './page.module.scss';
 import LoginBlock from './components/LoginBlock';
+import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 export default function Login() {
+    const searchParams = useSearchParams();
+    const alertMessage = searchParams?.get('alert');
+
+    useEffect(() => {
+        if (alertMessage === 'loginRequired') {
+            alert('로그인이 필요합니다.');
+        }
+    }, [alertMessage]);
+
     return (
         <div className={styles.mainContent}>
             <LoginBlock />
