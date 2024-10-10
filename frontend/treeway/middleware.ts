d784d84e48dff19239508189dbfe1f3607fb8aa7
정다운 <dawoon221@naver.com>
2024-10-10 11:08:17 +0900

    fix: 미들웨어 수정

--- frontend/treeway/middleware.ts
+++ frontend/treeway/middleware.ts
@@ -4,11 +4,11 @@ export const config = {
   matcher: ["/((?!fonts|components|$).*)"],
 };
 
-const protectedRoutes = ["/regist", "/createStoreImage", "/community", "/main/rating", "/main/industry", "/main/region", "/analyze"]; // 로그인 유저만 접근할 수 있는 페이지
-const publicRoutes = ["/login"]; // 로그인 유저는 접근할 수 없는 페이지
+const protectedRoutes = ["/createStoreImage", "/community", "/main/rating", "/main/industry", "/main/region", "/analyze"]; // 로그인 유저만 접근할 수 있는 페이지
+const publicRoutes = ["/login", "/regist"]; // 로그인 유저는 접근할 수 없는 페이지
 
 export function middleware(request: NextRequest) {
-    const isAuth = request.cookies.get('customUserDetails');
+    const isAuth = request.cookies.get('isAuth');
     const currentPath = request.nextUrl.pathname;
 
     if (!isAuth && protectedRoutes.some(route => currentPath.match(route))) {
