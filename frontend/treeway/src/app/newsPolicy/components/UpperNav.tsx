426f05e25c5e341449578418dbf74ca53234978d
정다운 <dawoon221@naver.com>
2024-10-10 21:40:56 +0900

    feat: 뉴스, 정책 지역 검색 추가

--- frontend/treeway/src/app/newsPolicy/components/UpperNav.tsx
+++ frontend/treeway/src/app/newsPolicy/components/UpperNav.tsx
@@ -1,14 +1,36 @@
+import Button from '@/app/common/Button';
 import styles from '../page.module.scss';
 import SearchBtn from './SearchBtn';
 import SortBox from './SortBox';
+import Swal from 'sweetalert2';
+import { regionOptions } from '@/../public/data/region';
+import { useState } from 'react';
+import Dropdown from '@/app/community/component/Dropdown';
 
 interface UpperNavProps {
   isNews: boolean;
   toggleNewsStatus: () => void;
   setSortCriteria: (criteria: string) => void;
+  setSelectedRegion: (regionId: number) => void;
 }
 
-export default function UpperNav({ isNews, toggleNewsStatus, setSortCriteria }: UpperNavProps) {
+export default function UpperNav({ isNews, toggleNewsStatus, setSortCriteria, setSelectedRegion }: UpperNavProps) {
+  const [selectedRegion, setLocalSelectedRegion] = useState<number>(0);
+
+  const handleSearch = () => {
+    if (selectedRegion === 0) {
+      Swal.fire({
+        title: '선택이 필요합니다!',
+        text: '지역을 선택해주세요.',
+        icon: 'warning',
+        confirmButtonText: '확인',
+      });
+      return;
+    }
+
+    setSelectedRegion(selectedRegion);
+  };
+
   return (
     <div className={styles.upperNav}>
       <div className={styles.leftBlock}>
@@ -24,6 +46,15 @@ export default function UpperNav({ isNews, toggleNewsStatus, setSortCriteria }: 
         >
           정책
         </button>
+      </div>
+      <div className={styles.centerBlock}>
+        <Dropdown
+          label="지역"
+          value={selectedRegion}
+          onChange={(e) => setLocalSelectedRegion(Number(e.target.value))}
+          options={regionOptions}
+        />
+        <Button content="검색" size="small" colorType="blue" onClick={handleSearch} />
       </div>
       <div className={styles.rightBlock}>
         <SortBox setSortCriteria={setSortCriteria} />
--- frontend/treeway/src/app/newsPolicy/page.tsx
+++ frontend/treeway/src/app/newsPolicy/page.tsx
@@ -11,107 +11,119 @@ export default function NewsPolicy() {
     const [isNews, setIsNews] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
     const [newsData, setNewsData] = useState([]);
-    const [policyData, setPolicyData] = useState([]); // 정책 데이터 상태
+    const [policyData, setPolicyData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [sortCriteria, setSortCriteria] = useState('Latest');
-
+    const [selectedRegion, setSelectedRegion] = useState<number>(0);
     const itemsPerPage = 4;
 
     const toggleNewsStatus = () => {
-        setIsNews(!isNews);
-        setCurrentPage(1); 
+      setIsNews(!isNews);
+      setCurrentPage(1);
     };
-
-    // 뉴스
+  
     useEffect(() => {
-        const fetchNewsData = async () => {
-            setLoading(true); 
-            try {
-                const response = await fetch('https://j11b107.p.ssafy.io/api/news');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch news data');
-                }
-                const data = await response.json();
-                setNewsData(data);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchNewsData();
-    }, []);
-
-    // 정책
-    useEffect(() => {
-        const fetchPolicyData = async () => {
-            setLoading(true);
-            try {
-                const response = await fetch('https://j11b107.p.ssafy.io/api/policy');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch policy data');
-                }
-                const data = await response.json();
-                setPolicyData(data);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchPolicyData();
-    }, []);
-
-    const sortData = (data: any[], criteria: string) => {
-        if (criteria === 'Latest') {
-            return data.sort((a, b) => new Date(b.createdAt ? b.createdAt : b.startDate).getTime() - new Date(a.createdAt? a.createdAt : a.startDate).getTime());
-        } else if (criteria === 'ScrapCount') {
-            return data.sort((a, b) => b.scrapCount - a.scrapCount);
+      const fetchNewsData = async () => {
+        setLoading(true);
+        try {
+          const response = await fetch('https://j11b107.p.ssafy.io/api/news');
+          if (!response.ok) {
+            throw new Error('Failed to fetch news data');
+          }
+          const data = await response.json();
+          setNewsData(data);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
         }
-        return data;
+      };
+  
+      fetchNewsData();
+    }, []);
+  
+    useEffect(() => {
+      const fetchPolicyData = async () => {
+        setLoading(true);
+        try {
+          const response = await fetch('https://j11b107.p.ssafy.io/api/policy');
+          if (!response.ok) {
+            throw new Error('Failed to fetch policy data');
+          }
+          const data = await response.json();
+          setPolicyData(data);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchPolicyData();
+    }, []);
+  
+    const sortData = (data: any[], criteria: string) => {
+      if (criteria === 'Latest') {
+        return data.sort((a, b) => new Date(b.createdAt ? b.createdAt : b.startDate).getTime() - new Date(a.createdAt ? a.createdAt : a.startDate).getTime());
+      } else if (criteria === 'ScrapCount') {
+        return data.sort((a, b) => b.scrapCount - a.scrapCount);
+      }
+      return data;
     };
-
+  
+    const filterByRegion = (data: any[], regionId: number) => {
+      if (regionId === 0) return data;
+      return data.filter(item => item.regionId === regionId);
+    };
+  
     // 현재 페이지에 보여줄 데이터
     const getCurrentPageData = (data: any[]) => {
-        const startIndex = (currentPage - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        return data.slice(startIndex, endIndex);
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      return data.slice(startIndex, endIndex);
     };
-
+  
     const sortedNewsData = sortData([...newsData], sortCriteria);
     const sortedPolicyData = sortData([...policyData], sortCriteria);
-
+  
+    const filteredNewsData = filterByRegion(sortedNewsData, selectedRegion);
+    const filteredPolicyData = filterByRegion(sortedPolicyData, selectedRegion);
+  
     if (loading) return <div>Loading...</div>;
-
+  
     return (
-        <div className={styles.background}>
-            <div className={styles.block}>
-                <div className={styles.postBlock}>
-                    <UpperNav isNews={isNews} toggleNewsStatus={toggleNewsStatus} setSortCriteria={setSortCriteria}/>
-                    {isNews ? (
-                        <NewsList newsData={getCurrentPageData(sortedNewsData)} />
-                    ) : (
-                        <PolicyList policyData={getCurrentPageData(sortedPolicyData)} />
-                    )}
-                </div>
-                <div>
-                    {isNews ? (
-                        <Pagenation 
-                            postCnt={newsData.length} 
-                            itemsPerPage={itemsPerPage} 
-                            onPageChange={(pageNumber) => setCurrentPage(pageNumber)} 
-                        />
-                    ) : (
-                        <Pagenation 
-                            postCnt={policyData.length} 
-                            itemsPerPage={itemsPerPage} 
-                            onPageChange={(pageNumber) => setCurrentPage(pageNumber)} 
-                        />
-                    )}
-                </div>
-            </div>
+      <div className={styles.background}>
+        <div className={styles.block}>
+          <div className={styles.postBlock}>
+            <UpperNav
+              isNews={isNews}
+              toggleNewsStatus={toggleNewsStatus}
+              setSortCriteria={setSortCriteria}
+              setSelectedRegion={setSelectedRegion}
+            />
+            {isNews ? (
+              <NewsList newsData={getCurrentPageData(filteredNewsData)} />
+            ) : (
+              <PolicyList policyData={getCurrentPageData(filteredPolicyData)} />
+            )}
+          </div>
+          <div>
+            {isNews ? (
+              <Pagenation
+                postCnt={filteredNewsData.length} 
+                itemsPerPage={itemsPerPage} 
+                onPageChange={(pageNumber) => setCurrentPage(pageNumber)}
+              />
+            ) : (
+              <Pagenation
+                postCnt={filteredPolicyData.length} 
+                itemsPerPage={itemsPerPage} 
+                onPageChange={(pageNumber) => setCurrentPage(pageNumber)}
+              />
+            )}
+          </div>
         </div>
+      </div>
     );
-}
+  }
+  
